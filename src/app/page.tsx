ea428e5ac8a347179fbf91880befb4e694fdabb3
yohanangulo <anguloyohan98@gmail.com>
2025-05-06 21:54:59 -0400

    Fix height

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,11 @@
 export default function Home() {
   return (
     <iframe
-      style={{ border: '1px solid rgba(0, 0, 0, 0.1)' }}
+      style={{
+        border: '1px solid rgba(0, 0, 0, 0.1)',
+        height: '100vh',
+      }}
       width="100%"
-      height="100dvh"
       src="https://embed.figma.com/proto/SxyfzfhejV9AyxqOPBe4Bu/Venley-APP-test?page-id=118%3A2798&node-id=130-1901&p=f&viewport=257%2C-2469%2C0.38&scaling=scale-down&content-scaling=fixed&starting-point-node-id=130%3A1901&show-proto-sidebar=1&embed-host=share"
       allowFullScreen
     ></iframe>
